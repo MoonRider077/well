b3aecb8dd2d096b274d190665bee4a60f734e237
Yusufjon Muxammadjonov <moonrider@Yusufjons-MacBook-Pro.local>
2024-07-05 10:48:41 +0500

    ss

--- src/components/home/accordion/FAQ.tsx
+++ src/components/home/accordion/FAQ.tsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import Accordion from './Accordion';
 import { useTranslation } from 'react-i18next';
 
+type AccordionItem = {
+  title: string;
+  answer: string;
+};
+
 const FAQ: React.FC = () => {
   const [openAccordionIndex, setOpenAccordionIndex] = useState<number | null>(null);
   const { t } = useTranslation('global');
@@ -10,7 +15,7 @@ const FAQ: React.FC = () => {
     setOpenAccordionIndex(openAccordionIndex === index ? null : index);
   };
 
-  const accordionItems = t('accordion.items', { returnObjects: true });
+  const accordionItems = t('accordion.items', { returnObjects: true }) as AccordionItem[]
 
   return (
     <div className="pt-[7.5rem] mx-auto max-w-screen-xl flex space-x-32">
@@ -18,7 +23,7 @@ const FAQ: React.FC = () => {
         <h2 className="font-semibold text-[40px] leading-[60px]">{t('accordion.title')}</h2>
       </div>
       <div className="w-3/4">
-        {accordionItems.map((item: any, index: number) => (
+        {accordionItems.map((item, index) => (
           <Accordion
             key={index}
             index={index}
