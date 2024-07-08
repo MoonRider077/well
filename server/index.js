2ced1bbf3e5f52032a22b40e6cbc6d8ba580cf8b
Yusufjon Muxammadjonov <moonrider@Yusufjons-MacBook-Pro.local>
2024-07-08 16:28:49 +0500

    sqqa

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.use(express_1.default.static(path_1.default.join(__dirname, '../build')));
 app.get('*', (req, res) => {
     res.sendFile(path_1.default.join(__dirname, '../build', 'index.html'));
